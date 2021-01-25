129f8521d46fd0d9d9f117060eed3e5eaab2d5c7
Dylan <dylan.brush@ontariotechu.net>
2021-01-24 20:28:15 -0500

    Character Customization Update
    New Features Added:
    Preview teams
    Preview & saving colours
    Changing skins
    Cape physics
    Accessories load into actual game
    Personalized Ferret experience incoming

---
+++ Assets/Scripts/CustomizationScripts/ManageBodyItems.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//USE PLAYER PREFS: RESEARCH ON DOCUMENTATION
+
+public class ManageBodyItems : MonoBehaviour
+{
+    public GameObject player;
+
+    public GameObject[] bodyItems;
+    private int bCurrentItem;
+    public GameObject[] hatItems;
+    private int hCurrentItem;
+    public GameObject[] maskItems;
+
+    private int mCurrentItem;
+    private bool b_Left = false;
+    private bool b_Right = false;
+
+    void Update()
+    {
+        if (b_Left)
+        {
+            player.transform.Rotate(0, 100 * Time.deltaTime, 0);
+        }
+        if (b_Right)
+        {
+            player.transform.Rotate(0, -100 * Time.deltaTime, 0);
+        }
+
+
+
+        for (int i = 0; i < 3; i++) {
+            switch (i)
+            {
+                case 0:
+                    Activate(0, bCurrentItem, bodyItems);
+                    break;
+                case 1:
+                    Activate(1, hCurrentItem, hatItems);
+                    break;
+                case 2:
+                    Activate(2, mCurrentItem, maskItems);
+                    break;
+            }
+        }
+    }
+
+    public void Activate(int part, int currentItem, GameObject[] items)
+    {
+        for (int c = 0; c < items.Length; c++)
+        {
+            if (c == currentItem)
+            {
+                items[c].SetActive(true);
+                items[c].transform.rotation = player.transform.rotation;
+            }
+            else
+            {
+                items[c].SetActive(false);
+            }
+        }
+    }
+
+    public void FinalizeChar()
+    {
+        PlayerPrefs.SetInt("Body", bCurrentItem);
+        PlayerPrefs.SetInt("Hat", hCurrentItem);
+        PlayerPrefs.SetInt("Mask", mCurrentItem);
+    }
+
+    public void SwitchBodyItems()
+    {
+        if (bCurrentItem == bodyItems.Length - 1)
+        {
+            bCurrentItem = 0;
+        }
+        else
+        {
+            bCurrentItem++;
+        }
+    }
+    public void SwitchHatItems()
+    {
+        if (hCurrentItem == hatItems.Length - 1)
+        {
+            hCurrentItem = 0;
+        }
+        else
+        {
+            hCurrentItem++;
+        }
+    }
+    public void SwitchMaskItems()
+    {
+        if (mCurrentItem == maskItems.Length - 1)
+        {
+            mCurrentItem = 0;
+        }
+        else
+        {
+            mCurrentItem++;
+        }
+    }
+
+    public void RotateLeft()
+    {
+        b_Left = true;
+    }
+    public void StopLeft()
+    {
+        b_Left = false;
+    }
+    public void RotateRight()
+    {
+        b_Right = true;
+    }
+    public void StopRight()
+    {
+        b_Right = false;
+    }
+}
---
+++ Assets/Scripts/CustomizationScripts/ManageColour.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManageColour : MonoBehaviour
+{
+    public GameObject player;
+
+    public GameObject TopHat;
+    public GameObject DragonWings;
+    public GameObject Cape;
+    public GameObject Mask;
+    public GameObject ArcherHat;
+    public GameObject Goggles;
+
+    public RawImage ri_RedTeam;
+    public RawImage ri_BlueTeam;
+
+    private int currentMaterial = 0;
+    private int i_Skin = 0;
+    private int i_Colour = 0;
+    public string s_TeamColour = "Red";
+
+    private bool b_TeamChanger = false;
+
+    private void Start()
+    {
+        ApplyColour();
+    }
+
+    public void SaveColour()
+    {
+        if (currentMaterial == 0)
+        {
+            switch(i_Colour)
+            {
+                case 0:
+                    ri_RedTeam.texture = Resources.Load<Texture2D>("UI/redBG");
+                    break;
+                case 1:
+                    ri_RedTeam.texture = Resources.Load<Texture2D>("UI/orangeBG");
+                    break;
+                case 2:
+                    ri_RedTeam.texture = Resources.Load<Texture2D>("UI/yellowBG");
+                    break;
+            }
+        }
+        else
+        {
+            switch (i_Colour)
+            {
+                case 0:
+                    ri_BlueTeam.texture = Resources.Load<Texture2D>("UI/blueBG");
+                    break;
+                case 1:
+                    ri_BlueTeam.texture = Resources.Load<Texture2D>("UI/greenBG");
+                    break;
+                case 2:
+                    ri_BlueTeam.texture = Resources.Load<Texture2D>("UI/purpleBG");
+                    break;
+            }
+        }
+
+        if (currentMaterial == 0)
+        {
+            PlayerPrefs.SetInt("RedColour", i_Colour);
+        }
+        else
+        {
+
+            PlayerPrefs.SetInt("BlueColour", i_Colour);
+        }
+    }
+
+    public void SwitchSkin()
+    {
+        if (i_Skin == 2)
+        {
+            i_Skin = 0;
+        }
+        else
+        {
+            i_Skin++;
+        }
+
+        switch (i_Skin)
+        {
+            case 0:
+                player.GetComponent<SkinnedMeshRenderer>().material = Resources.Load<Material>("Materials/FerretBaseColour");
+                break;
+            case 1:
+                player.GetComponent<SkinnedMeshRenderer>().material = Resources.Load<Material>("Materials/FerretAlbino");
+                break;
+            case 2:
+                player.GetComponent<SkinnedMeshRenderer>().material = Resources.Load<Material>("Materials/FerretDarkBrown");
+                break;
+        }
+        PlayerPrefs.SetInt("Skin", i_Skin);
+    }
+
+    public void SwitchColour()
+    {
+        if (i_Colour == 2)
+        {
+            i_Colour = 0;
+        }
+        else
+        {
+            i_Colour++;
+        }
+
+        ApplyColour();
+    }
+
+    public void SwitchTeams()
+    {
+        if (currentMaterial == 1)
+        {
+            currentMaterial = 0;
+        }
+        else
+        {
+            currentMaterial++;
+        }
+
+        ApplyColour();
+    }
+
+    public void ApplyColour()
+    {
+        if (currentMaterial == 0)
+        {
+            switch (i_Colour)
+            {
+                case 0:
+                    s_TeamColour = "Red";
+                    break;
+                case 1:
+                    s_TeamColour = "Orange";
+                    break;
+                case 2:
+                    s_TeamColour = "Yellow";
+                    break;
+            }
+        }
+        else
+        {
+            switch (i_Colour)
+            {
+                case 0:
+                    s_TeamColour = "Blue";
+                    break;
+                case 1:
+                    s_TeamColour = "Green";
+                    break;
+                case 2:
+                    s_TeamColour = "Purple";
+                    break;
+            }
+        }
+
+        TopHat.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/Tophat" + s_TeamColour);
+        DragonWings.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/Dragon_Wings" + s_TeamColour);
+        Cape.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/Cape" + s_TeamColour);
+        Mask.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/Mask" + s_TeamColour);
+        ArcherHat.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/ArcherHat" + s_TeamColour);
+    }
+}
