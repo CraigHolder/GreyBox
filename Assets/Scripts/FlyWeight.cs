632851f7c59aa6133be6e1a98d7624d2c608e88c
CraigHolder <craig.holder@ontariotechu.net>
2021-02-25 16:59:34 -0500

    Bug Fixes and Speakers

--- Assets/Scripts/FlyWeight.cs
+++ Assets/Scripts/FlyWeight.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Flyweight", order = 1)]
 public class FlyWeight : ScriptableObject
 {
-    public Speakers.SpeakerState e_speakerstate = Speakers.SpeakerState.Off;
+    public Speakers.SpeakerState e_speakerstate;
     public Subject S_Notifier = new Subject();
     Achievments achievmentobserver = new Achievments();
 
     public BounceObjCommand c_objbounce;
 
-    public void Awake()
+    //public void Awake()
+    //{
+    //    S_Notifier.AddObserver(achievmentobserver);
+    //    e_speakerstate = Speakers.SpeakerState.Off;
+    //}
+    public void OnEnable()
     {
         c_objbounce = new BounceObjCommand();
         S_Notifier.AddObserver(achievmentobserver);
+        e_speakerstate = Speakers.SpeakerState.Off;
     }
-
 }
