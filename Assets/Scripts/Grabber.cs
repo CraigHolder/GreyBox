7beab3eaa90411c6db67363aa70996f4b772c008
Dylan <dylan.brush@ontariotechu.net>
2021-03-26 18:19:00 -0400

    Revert "Merge remote-tracking branch 'origin/Craig-Gameplay-Electric-Boogaloo' into Dylan"
    This reverts commit a27b0cbb421fdfa40d54bfdddf260dc5ff26cfd6, reversing
    changes made to 492953a427873745004abf02d87c2b0a4120a113.

--- Assets/Scripts/Grabber.cs
+++ Assets/Scripts/Grabber.cs
@@ -123,7 +123,6 @@ public class Grabber : MonoBehaviour
     {
         if(collision.gameObject.tag == "Grab" && obj_curritem == null)
         {
-            t_itemhandle.position = obj_mouth.transform.position;
             obj_curritem = collision.gameObject;
             //obj_curritem.GetComponent<Rigidbody>().isKinematic = true;
             Vector3 closestpoint = collision.ClosestPoint(t_itemhandle.position);
