544313b212f54b3f02c648b062d004a8adf3434e
Dylan <dylan.brush@ontariotechu.net>
2021-03-28 15:56:19 -0400

    lobbys started

--- Assets/Scripts/UI scripts/LobbyScript.cs
+++ Assets/Scripts/UI scripts/LobbyScript.cs
@@ -1,9 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class LobbyScript : MonoBehaviour
 {
+    public struct Server
+    {
+        public string name;
+        public string pass;
+        public string ID;
+        public bool isPrivate;
+        public bool isJoining;
+
+    }
+
+    public Server toJoin;
+    public Server toHost;
+
+    //Hosting
+    public Toggle t_IsPriv;
+    public TMP_InputField IF_LobbyName;
+    public TMP_InputField IF_LobbyPass;
+
+    //Joining:
+    public Server tempServer;
+    public TMP_InputField IF_JoinPass;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +39,26 @@ public class LobbyScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void StartServer()
+    {
+
+        toHost.name = IF_LobbyName.text;
+        toHost.pass = IF_LobbyPass.text;
+        toHost.isPrivate = t_IsPriv.isOn;
+        toHost.isJoining = false;
+
+    }
+
+    void JoinServer()
+    {
+        toJoin.name = tempServer.name;
+        toJoin.ID = tempServer.ID;
+        toJoin.pass = IF_LobbyPass.text;
+        toJoin.isPrivate = tempServer.isPrivate;
+        toJoin.isJoining = true;
+
+
     }
 }
